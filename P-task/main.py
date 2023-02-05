1d0ee4bf52ff293cb7b6317df18329bb7e989c3b
Maksim <112699885+Maxkaz86@users.noreply.github.com>
2023-02-05 20:43:56 +0300

    Update main.py

--- P-task/main.py
+++ P-task/main.py
@@ -1,7 +1,7 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 just_pc = list(per_cent.values())
 amount = int(input('money: '))
-sum_in = amount * just_pc[0], amount * just_pc[1], amount * just_pc[2], amount * just_pc[3]
+sum_in = amount * just_pc[0] / 100, amount * just_pc[1] / 100, amount * just_pc[2] / 100, amount * just_pc[3] / 100
 deposit = list(map(int, sum_in))
 print(deposit)
 print('Максимальная сумма, которую вы можете заработать - %d у.е.' % (max(deposit)))
