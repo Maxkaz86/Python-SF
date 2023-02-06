dc54547ff9f301605591a5db2df836aff44a3b67
Maksim <112699885+Maxkaz86@users.noreply.github.com>
2023-02-06 19:53:26 +0300

    first tasknew

--- P-task/main.py
+++ P-task/main.py
@@ -1,7 +1,13 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-just_pc = list(per_cent.values())
+just_val = list(per_cent.values())
+pc_con = []
 amount = int(input('money: '))
-sum_in = amount * just_pc[0] / 100, amount * just_pc[1] / 100, amount * just_pc[2] / 100, amount * just_pc[3] / 100
-deposit = list(map(int, sum_in))
-print(deposit)
+deposit = []
+for pc in just_val:
+    pc_con.append(round(pc / 100, 3))
+for i in pc_con:
+    deposit.append(int(amount * i))
+# sum_in = amount * just_pc[0] / 100, amount * just_pc[1] / 100, amount * just_pc[2] / 100, amount * just_pc[3] / 100
+# deposit = list(map(int, sum_in))
+# print(deposit)
 print('Максимальная сумма, которую вы можете заработать - %d у.е.' % (max(deposit)))
---
+++ P-task/tasks.py
@@ -0,0 +1,11 @@
+# string = '{1}, {2}, {0}'.format('Ivan', 'Petya', 'Katya')
+# print(string)
+# empty_m = []
+# numbers = [10,12,122,11]
+# amount = int(input('введите число '))
+# for n in numbers:
+#     empty_m.append(amount * n)
+# print(empty_m)
+i = ['q', 'w', 'e' ,' t']
+i.insert(3, 'r')
+print(i)
